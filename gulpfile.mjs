09ac86debd7944bdec1d3471dc8750631deaabb8
Nabeel Shahzad <nabeelio@users.noreply.github.com>
2025-03-11 14:36:19 -0500

    Zip generation

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,10 +1,13 @@
 import dotconfig from '@dotenvx/dotenvx'
-import { deleteAsync } from 'del'
+import { deleteSync as del } from 'del'
 import { dest, series, src, watch } from 'gulp'
 import eslint from 'gulp-eslint-new'
 import ts from 'gulp-typescript'
 import prettier from 'gulp-prettier'
+import zip from 'gulp-zip'
+import tap from 'gulp-tap'
 import minify from 'gulp-minify'
+import fs from 'fs'
 
 dotconfig.config()
 
@@ -13,27 +16,38 @@ dotconfig.config()
  * Don't modify this directly, use the environment variables
  */
 const paths = {
-  src: './src',
-  dist: './dist',
-
   /**
    * ACARS scripts/config directory. This, by default, points to the home directory
    * But you can change this to point to a local directory
    */
   acars: process.env.ACARS_SCRIPTS_PATH,
+
+  src: './src',
+  out: './dist',
+  export: './dist',
 }
 
 /**
  * Build the project, copy the appropriate files over
  * @public
  */
-export const build = series(buildTsTask, copyPackageTask)
+export const build = series(
+  buildTsTask,
+  copyPackageJsonTask,
+)
+
+/**
+ * Clean the build directories
+ * @public
+ */
+export const clean = cleanTask
 
 /**
  * Build a distribution zip file, which can be easily uploaded
  * @public
  */
 export const dist = series(
+  clean,
   build,
   buildZipTask,
 )
@@ -43,16 +57,7 @@ export const dist = series(
  * documents/vmsacars/data/<profile>/config directory
  * @public
  */
-export const local = localTask
-
-
-/**
- * The default action
- * @default
- * @public
- */
-export default build
-
+export const local = localBuildTask
 
 /**
  * The build steps that run from the csproj
@@ -64,8 +69,16 @@ export const csbuild = series(
     paths.acars = '../Content/config/default'
   },
   build,
-  copyFilesToScriptsPathTask,
+  copyFilesToAcarsPathTask,
 )
+
+
+/**
+ * The default action
+ * @default
+ * @public
+ */
+export default build
 
 /**
  *
@@ -75,14 +88,19 @@ export const csbuild = series(
 
 /**
  * Configure the ts transpilation
+ *
  */
 const tsProject = ts.createProject('tsconfig.json')
 
 /**
  * Build the Typescript files
- * @returns {module:stream.Stream.Transform | *}
  */
-async function buildTsTask() {
+function buildTsTask() {
+  // ensure the dist directory exists
+  if (!fs.existsSync(paths.out)) {
+    fs.mkdirSync(paths.out)
+  }
+
   let pipeline = tsProject.src()
     .pipe(eslint())
     .pipe(eslint.failAfterError())
@@ -96,26 +114,24 @@ async function buildTsTask() {
     mangle: false,
   }))*/
 
-  pipeline = pipeline
-    .pipe(dest(paths.dist))
+  pipeline = pipeline.pipe(dest(paths.out))
 
   return pipeline
 }
 
 /**
+ * This copies the package.json file to the output directory
  *
- * @returns {*}
  */
-async function copyPackageTask() {
+function copyPackageJsonTask() {
   return src([paths.src + '/package.json'])
-    .pipe(dest(paths.dist))
+    .pipe(dest(paths.out))
 }
 
 /**
  * Copy the files from dist into ACARS_SCRIPTS_PATH
- *
  */
-export async function copyFilesToScriptsPathTask() {
+function copyFilesToAcarsPathTask() {
   console.log(`Copying files to ${paths.acars}`)
 
   return src(
@@ -123,48 +139,46 @@ export async function copyFilesToScriptsPathTask() {
       './**/*',
       '!node_modules/**/*',
     ],
-    { 'cwd': paths.dist },
+    { 'cwd': paths.out },
   )
     .pipe(dest(paths.acars))
 }
 
 
 /**
- * TODO: Build the distribution zip file
+ * Build the zip that should get uploaded
  */
 function buildZipTask() {
+  console.log('Writing zip named ' + process.env.ACARS_DIST_ZIP)
+  if (!fs.existsSync(paths.export)) {
+    fs.mkdirSync(paths.export)
+  }
 
-}
-
-
-/**
- * Watch the src folder for updates, compile them and then copy them
- * to the config directory. ACARS should auto-reload
- */
-export async function testingTask() {
-  watch('src/', {
-    ignoreInitial: false,
-    delay: 500,
-  }, series(build, copyFilesToScriptsPathTask))
+  return src(paths.out + '/**/*', { base: paths.out })
+    /*.pipe(tap(function (file) {
+      console.log('file: ' + file.path)
+    }))*/
+    .pipe(zip(process.env.ACARS_DIST_ZIP, { buffer: true}))
+    .pipe(dest(paths.export))
 }
 
 /**
  * Watch the files and then build and copy them to the documents directory
  */
-export async function localTask() {
-  return watch('src/', { ignoreInitial: false }, series(build, copyFilesToScriptsPathTask), function() {
-    cb()
-  })
+function localBuildTask() {
+  return watch(
+    paths.src,
+    { ignoreInitial: false },
+    series(build, copyFilesToAcarsPathTask)
+  )
 }
 
 /**
  * Clean up the /dest directory
  */
-export async function cleanTask() {
-  try {
-    await deleteAsync([paths.dist])
-    await Promise.resolve()
-  } catch (e) {
-    console.log(e)
-  }
+async function cleanTask() {
+  return del([
+    paths.out,
+    paths.export + '/' + process.env.ACARS_DIST_ZIP
+  ])
 }
